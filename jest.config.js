598ef5c434fea55a950d896def220712df76dfad
Soma Natarajan <soma.natarajan@oasisworks.com>
2020-06-19 11:06:55 -0400

    fixed test config

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,4 @@
 module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
-  coverageDirectory: 'coverage',
-
-  // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/(?!@foo)'],
-
-  globals: {
-    'ts-jest': {
-      tsConfigFile: 'tsconfig.json',
-      enableTsDiagnostics: true,
-    },
-  },
-
-  moduleFileExtensions: ['js'],
-
-  // A map from regular expressions to module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    '@src/(.*)': '<rootDir>/build/src/$1',
-    database: '<rootDir>/src/lib/database',
-  },
-
+  preset: 'ts-jest',
   testEnvironment: 'node',
-
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-
-  testPathIgnorePatterns: ['utils/utils.js', 'build/tests/utils.js'],
-
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!@foo)'],
-  testTimeout: 5000,
 };
