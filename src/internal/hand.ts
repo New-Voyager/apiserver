82ef499187f8389ea95c3e255006af8da5918fe2
sanjay <sanjay@sanjay-Lenovo-Yoga-2-11.sanjay.com>
2020-07-08 15:54:55 +0530

    validateHandData Function

--- src/internal/hand.ts
+++ src/internal/hand.ts
@@ -6,115 +6,126 @@ import {
   GameType,
 } from '@src/entity/hand';
 
-/**
- * Hand Server API class
- */
-class HandServerAPIs {
-  public async saveHand(req: any, resp: any) {
-    const registerPayload = req.body;
-
-    /**
-     * Checking Whether all required data is available
-     */
-    const errors = new Array<string>();
+function validateHandData(handData: any): Array<string>{
+  const errors = new Array<string>();
     try {
-      if (!registerPayload.ClubId) {
+      if (!handData.ClubId) {
         errors.push('ClubId is missing');
       }
-      if (!registerPayload.GameNum) {
+      if (!handData.GameNum) {
         errors.push('GameNum is missing');
       }
-      if (!registerPayload.HandNum) {
+      if (!handData.HandNum) {
         errors.push('HandNum is missing');
       }
-      if (!registerPayload.Players || registerPayload.Players.length === 0) {
+      if (!handData.Players || handData.Players.length === 0) {
         errors.push('Players are missing');
       }
-      if (!registerPayload.GameType) {
+      if (!handData.GameType) {
         errors.push('GameType is missing');
       } else {
         if (
-          registerPayload.GameType !== GameType[GameType.HOLDEM] &&
-          registerPayload.GameType !== GameType[GameType.OMAHA] &&
-          registerPayload.GameType !== GameType[GameType.OMAHA_HILO] &&
-          registerPayload.GameType !== GameType[GameType.UNKNOWN]
+          handData.GameType !== GameType[GameType.HOLDEM] &&
+          handData.GameType !== GameType[GameType.OMAHA] &&
+          handData.GameType !== GameType[GameType.OMAHA_HILO] &&
+          handData.GameType !== GameType[GameType.UNKNOWN]
         ) {
           errors.push('invalid GameType field');
         }
       }
-      if (!registerPayload.StartedAt) {
+      if (!handData.StartedAt) {
         errors.push('StartedAt is missing');
       }
-      if (!registerPayload.EndedAt) {
+      if (!handData.EndedAt) {
         errors.push('EndedAt is missing');
       }
-      if (!registerPayload.Result) {
+      if (!handData.Result) {
         errors.push('Result is missing');
       } else {
         if (
-          !registerPayload.Result.pot_winners ||
-          registerPayload.Result.pot_winners.length === 0
+          !handData.Result.pot_winners ||
+          handData.Result.pot_winners.length === 0
         ) {
           errors.push('pot_winners is missing');
         }
       }
-      if (!registerPayload.Result.won_at) {
+      if (!handData.Result.won_at) {
         errors.push('won_at is missing');
       } else {
         if (
-          registerPayload.Result.won_at !== WonAtStatus[WonAtStatus.FLOP] &&
-          registerPayload.Result.won_at !== WonAtStatus[WonAtStatus.PREFLOP] &&
-          registerPayload.Result.won_at !== WonAtStatus[WonAtStatus.RIVER] &&
-          registerPayload.Result.won_at !== WonAtStatus[WonAtStatus.SHOWDOWN] &&
-          registerPayload.Result.won_at !== WonAtStatus[WonAtStatus.TURN]
+          handData.Result.won_at !== WonAtStatus[WonAtStatus.FLOP] &&
+          handData.Result.won_at !== WonAtStatus[WonAtStatus.PREFLOP] &&
+          handData.Result.won_at !== WonAtStatus[WonAtStatus.RIVER] &&
+          handData.Result.won_at !== WonAtStatus[WonAtStatus.SHOWDOWN] &&
+          handData.Result.won_at !== WonAtStatus[WonAtStatus.TURN]
         ) {
           errors.push('invalid Won_at field');
         }
       }
-      if (!registerPayload.Result.pot_winners) {
+      if (!handData.Result.pot_winners) {
         errors.push('pot_winners field is missing');
       }
-      if (!('showdown' in registerPayload.Result)) {
+      if (!('showdown' in handData.Result)) {
         errors.push('showdown field is missing');
       } else {
-        if (registerPayload.Result.showdown) {
-          if (!registerPayload.Result.total_pot) {
+        if (handData.Result.showdown) {
+          if (!handData.Result.total_pot) {
             errors.push('total_pot field is missing');
           }
-          if (registerPayload.GameType === GameType[GameType.OMAHA_HILO]) {
-            if (!registerPayload.Result.hi_winning_cards) {
+          if (handData.GameType === GameType[GameType.OMAHA_HILO]) {
+            if (!handData.Result.hi_winning_cards) {
               errors.push('hi_winning_cards field is missing');
             }
-            if (!registerPayload.Result.hi_winning_rank) {
+            if (!handData.Result.hi_winning_rank) {
               errors.push('hi_winning_rank field is missing');
             }
-            if (!registerPayload.Result.lo_winning_cards) {
+            if (!handData.Result.lo_winning_cards) {
               errors.push('lo_winning_cards field is missing');
             }
-            if (!registerPayload.Result.lo_winning_rank) {
+            if (!handData.Result.lo_winning_rank) {
               errors.push('lo_winning_rank field is missing');
             }
           } else {
-            if (!registerPayload.Result.winning_cards) {
+            if (!handData.Result.winning_cards) {
               errors.push('winning_cards field is missing');
             }
-            if (!registerPayload.Result.rank_num) {
+            if (!handData.Result.rank_num) {
               errors.push('rank_num field is missing');
             }
           }
         }
       }
     } catch (err) {
-      resp.status(500).send('Internal service error');
-      return;
+      errors.push('INTERNAL');
+      return errors;
     }
+  return errors;
+}
+
+/**
+ * Hand Server API class
+ */
+class HandServerAPIs {
+  public async saveHand(req: any, resp: any) {
+    const handData = req.body;
+
+    /**
+     * Checking for errors
+     */
+    let errors = new Array<string>();
+    errors = validateHandData(handData);
 
     /**
      * If any data is missing throwing errors
      */
     if (errors.length) {
-      resp.status(500).send(JSON.stringify(errors));
-      return;
+      if(errors[errors.length - 1] === 'INTERNAL' ){
+        resp.status(500).send('Internal service error');
+        return;
+      }else{
+        resp.status(500).send(JSON.stringify(errors));
+        return;
+      }
     }
 
     /**
@@ -125,47 +136,47 @@ class HandServerAPIs {
       const handWinnersRepository = getRepository(HandWinners);
 
       const handHistory = new HandHistory();
-      const wonAt: string = registerPayload.Result.won_at;
-      const gameType: string = registerPayload.GameType;
+      const wonAt: string = handData.Result.won_at;
+      const gameType: string = handData.GameType;
 
       /**
        * Assigning values and saving hand history
        */
-      handHistory.clubId = registerPayload.ClubId;
-      handHistory.gameNum = registerPayload.GameNum;
-      handHistory.handNum = registerPayload.HandNum;
+      handHistory.clubId = handData.ClubId;
+      handHistory.gameNum = handData.GameNum;
+      handHistory.handNum = handData.HandNum;
       handHistory.gameType = GameType[gameType];
       handHistory.wonAt = WonAtStatus[wonAt];
-      handHistory.showDown = registerPayload.Result.showdown;
-      if (registerPayload.Result.showdown) {
-        if (registerPayload.GameType === GameType[GameType.OMAHA_HILO]) {
-          handHistory.winningCards = registerPayload.Result.hi_winning_cards.join(
+      handHistory.showDown = handData.Result.showdown;
+      if (handData.Result.showdown) {
+        if (handData.GameType === GameType[GameType.OMAHA_HILO]) {
+          handHistory.winningCards = handData.Result.hi_winning_cards.join(
             ', '
           );
-          handHistory.winningRank = registerPayload.Result.hi_winning_rank;
-          handHistory.loWinningCards = registerPayload.Result.lo_winning_cards.join(
+          handHistory.winningRank = handData.Result.hi_winning_rank;
+          handHistory.loWinningCards = handData.Result.lo_winning_cards.join(
             ', '
           );
-          handHistory.loWinningRank = registerPayload.Result.lo_winning_rank;
+          handHistory.loWinningRank = handData.Result.lo_winning_rank;
         } else {
-          handHistory.winningCards = registerPayload.Result.winning_cards.join(
+          handHistory.winningCards = handData.Result.winning_cards.join(
             ', '
           );
-          handHistory.winningRank = registerPayload.Result.rank_num;
+          handHistory.winningRank = handData.Result.rank_num;
         }
-        handHistory.totalPot = registerPayload.Result.total_pot;
+        handHistory.totalPot = handData.Result.total_pot;
       }
-      handHistory.timeStarted = registerPayload.StartedAt;
-      handHistory.timeEnded = registerPayload.EndedAt;
-      handHistory.data = JSON.stringify(registerPayload);
+      handHistory.timeStarted = handData.StartedAt;
+      handHistory.timeEnded = handData.EndedAt;
+      handHistory.data = JSON.stringify(handData);
       await handHistoryRepository.save(handHistory);
 
       /**
        * Assigning values and saving hand winners
        */
-      if (registerPayload.Result.showdown) {
-        if (registerPayload.GameType === GameType[GameType.OMAHA_HILO]) {
-          await registerPayload.Result.pot_winners[0].hi_winners.forEach(
+      if (handData.Result.showdown) {
+        if (handData.GameType === GameType[GameType.OMAHA_HILO]) {
+          await handData.Result.pot_winners[0].hi_winners.forEach(
             async (winner: {
               winning_cards: Array<string>;
               rank_num: number;
@@ -173,9 +184,9 @@ class HandServerAPIs {
               received: number;
             }) => {
               const handWinners = new HandWinners();
-              handWinners.clubId = registerPayload.ClubId;
-              handWinners.gameNum = registerPayload.GameNum;
-              handWinners.handNum = registerPayload.HandNum;
+              handWinners.clubId = handData.ClubId;
+              handWinners.gameNum = handData.GameNum;
+              handWinners.handNum = handData.HandNum;
               handWinners.winningCards = winner.winning_cards.join(', ');
               handWinners.winningRank = winner.rank_num;
               handWinners.playerId = winner.player;
@@ -184,7 +195,7 @@ class HandServerAPIs {
             }
           );
 
-          await registerPayload.Result.pot_winners[0].lo_winners.forEach(
+          await handData.Result.pot_winners[0].lo_winners.forEach(
             async (winner: {
               winning_cards: Array<string>;
               rank_num: number;
@@ -192,9 +203,9 @@ class HandServerAPIs {
               received: number;
             }) => {
               const handWinners = new HandWinners();
-              handWinners.clubId = registerPayload.ClubId;
-              handWinners.gameNum = registerPayload.GameNum;
-              handWinners.handNum = registerPayload.HandNum;
+              handWinners.clubId = handData.ClubId;
+              handWinners.gameNum = handData.GameNum;
+              handWinners.handNum = handData.HandNum;
               handWinners.winningCards = winner.winning_cards.join(', ');
               handWinners.winningRank = winner.rank_num;
               handWinners.playerId = winner.player;
@@ -204,7 +215,7 @@ class HandServerAPIs {
             }
           );
         } else {
-          await registerPayload.Result.pot_winners[0].winners.forEach(
+          await handData.Result.pot_winners[0].winners.forEach(
             async (winner: {
               winning_cards: Array<string>;
               rank_num: number;
@@ -212,9 +223,9 @@ class HandServerAPIs {
               received: number;
             }) => {
               const handWinners = new HandWinners();
-              handWinners.clubId = registerPayload.ClubId;
-              handWinners.gameNum = registerPayload.GameNum;
-              handWinners.handNum = registerPayload.HandNum;
+              handWinners.clubId = handData.ClubId;
+              handWinners.gameNum = handData.GameNum;
+              handWinners.handNum = handData.HandNum;
               handWinners.winningCards = winner.winning_cards.join(', ');
               handWinners.winningRank = winner.rank_num;
               handWinners.playerId = winner.player;
@@ -224,12 +235,12 @@ class HandServerAPIs {
           );
         }
       } else {
-        await registerPayload.Result.pot_winners[0].winners.forEach(
+        await handData.Result.pot_winners[0].winners.forEach(
           async (winner: {player: string; received: number}) => {
             const handWinners = new HandWinners();
-            handWinners.clubId = registerPayload.ClubId;
-            handWinners.gameNum = registerPayload.GameNum;
-            handWinners.handNum = registerPayload.HandNum;
+            handWinners.clubId = handData.ClubId;
+            handWinners.gameNum = handData.GameNum;
+            handWinners.handNum = handData.HandNum;
             handWinners.playerId = winner.player;
             handWinners.received = winner.received;
             await handWinnersRepository.save(handWinners);
