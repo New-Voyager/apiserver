cd6d299f87b23587040e2eeb078d84b2525d59c5
Soma Natarajan <soma.voyager@gmail.com>
2020-12-09 07:51:30 -0500

    copied from backup

---
+++ src/entity/reward.ts
@@ -0,0 +1,127 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  JoinColumn,
+  ManyToOne,
+  Index,
+} from 'typeorm';
+
+import {Player} from './player';
+import {RewardType, ScheduleType} from './types';
+import {Club} from './club';
+import {PokerGame} from './game';
+
+@Entity({name: 'reward'})
+export class Reward {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @ManyToOne(() => Club, clubId => clubId.id, {eager: true, nullable: false})
+  @JoinColumn({name: 'club_id'})
+  public clubId!: Club;
+
+  @Column({name: 'name', nullable: false})
+  public name!: string;
+
+  @Column({name: 'type', nullable: false})
+  public type!: RewardType;
+
+  @Column({name: 'amount', nullable: false})
+  public amount!: number;
+
+  @Column({name: 'schedule', nullable: false})
+  public schedule!: ScheduleType;
+
+  @Column({name: 'min_rank', nullable: true})
+  public minRank!: number;
+
+  @Column({name: 'start_hour', nullable: false})
+  public startHour!: number;
+
+  @Column({name: 'end_hour', nullable: false})
+  public endHour!: number;
+}
+
+@Entity({name: 'game_reward_tracking'})
+export class GameRewardTracking {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @ManyToOne(() => Club, clubId => clubId.id, {eager: true, nullable: false})
+  @JoinColumn({name: 'club_id'})
+  public clubId!: Club;
+
+  @Column({name: 'day', nullable: false})
+  public day!: Date;
+
+  @Column({name: 'hh_rank', nullable: false})
+  public hhRank!: number;
+
+  @ManyToOne(() => PokerGame, gameId => gameId.id, {
+    eager: true,
+    nullable: false,
+  })
+  @JoinColumn({name: 'game_id'})
+  public gameId!: PokerGame;
+
+  @ManyToOne(() => Player, playerId => playerId.id, {
+    eager: true,
+    nullable: false,
+  })
+  @JoinColumn({name: 'player_id'})
+  public playerId!: Player;
+
+  @Column({name: 'player_cards', nullable: false})
+  public playerCards!: string;
+
+  @Column({name: 'board_cards', nullable: false})
+  public boardCards!: string;
+
+  @Column({name: 'high_hand', nullable: false})
+  public highHand!: string;
+
+  @Column({name: 'hand_num', nullable: false})
+  public handNum!: number;
+
+  @Column({name: 'hit_time', nullable: false})
+  public hitTime!: Date;
+
+  @Column({name: 'start_time', nullable: true})
+  public startTime!: Date;
+
+  @Column({name: 'end_time', nullable: true})
+  public endTime!: Date;
+
+  @Column({name: 'active', nullable: true})
+  public active!: boolean;
+}
+
+@Entity({name: 'game_reward'})
+export class GameReward {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @ManyToOne(() => PokerGame, gameId => gameId.id, {
+    eager: true,
+    nullable: false,
+  })
+  @JoinColumn({name: 'game_id'})
+  public gameId!: PokerGame;
+
+  @ManyToOne(() => Reward, rewardId => rewardId.id, {
+    eager: true,
+    nullable: false,
+  })
+  @JoinColumn({name: 'reward_id'})
+  public rewardId!: Reward;
+
+  @ManyToOne(
+    () => GameRewardTracking,
+    rewardTrackingId => rewardTrackingId.id,
+    {eager: true, nullable: false}
+  )
+  @JoinColumn({name: 'reward_tracking_id'})
+  public rewardTrackingId!: GameRewardTracking;
+}
