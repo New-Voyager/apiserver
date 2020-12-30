16f61c2971240aa1d1c824146a5e5c89a2351966
sanjaysans <smskumar515@gmail.com>
2020-12-30 16:12:10 +0530

    seatchange tests

--- tests/seatchange.test.ts
+++ tests/seatchange.test.ts
@@ -100,6 +100,12 @@ async function createClubWithMembers(
   return [ownerUuid, clubCode, playerUuids];
 }
 
+function sleep(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
 const GAMESERVER_API = `http://localhost:${PORT_NUMBER}/internal`;
 
 describe('Tests: seat change APIs', () => {
@@ -118,7 +124,7 @@ describe('Tests: seat change APIs', () => {
       {
         name: 'player1',
         devId: 'test321',
-      }
+      },
     ]);
     const player1 = ownerId;
     const player2 = players[0];
@@ -131,7 +137,7 @@ describe('Tests: seat change APIs', () => {
 
     // join a game
     await gameutils.joinGame(player1, game.gameCode, 1);
-    await gameutils.joinGame(player2, game.gameCode, 1);
+    await gameutils.joinGame(player2, game.gameCode, 2);
 
     // buyin
     await gameutils.buyin(player1, game.gameCode, 100);
@@ -147,80 +153,32 @@ describe('Tests: seat change APIs', () => {
 
     // get all requested seat changes
     const resp3 = await gameutils.seatChangeRequests(ownerId, game.gameCode);
-    console.log(resp3);
     expect(resp3).toHaveLength(2);
-    resp3.forEach(element => {
-      expect(element.seatChangeConfirmed).toBe(false);
-    });
-
-    // // confirm seat change
-    // const resp4 = await gameutils.confirmSeatChange(
-    //   player1Id,
-    //   game.gameCode,
-    //   2
-    // );
-    // expect(resp4).toBe(true);
-  });
-
-  test.skip('confirm seat change', async () => {
-    // Create club and owner
-    const [clubCode, ownerId] = await clubutils.createClub('brady', 'yatzee');
-
-    // create gameserver and game
-    await createGameServer('1.2.1.6');
-    const game = await gameutils.configureGame(
-      ownerId,
-      clubCode,
-      holdemGameInput
-    );
-
-    // Create players
-    const player1Id = await clubutils.createPlayer('player1', 'abc1234');
-    const player2Id = await clubutils.createPlayer('player2', 'abc1235');
-    const player3Id = await clubutils.createPlayer('player3', 'abc1236');
-    const player4Id = await clubutils.createPlayer('player4', 'abc1237');
-
-    // joins the club
-    await clubutils.playerJoinsClub(clubCode, player1Id);
-    await clubutils.playerJoinsClub(clubCode, player2Id);
-    await clubutils.playerJoinsClub(clubCode, player3Id);
-    await clubutils.playerJoinsClub(clubCode, player4Id);
-
-    // approve joining
-    await clubutils.approvePlayer(clubCode, ownerId, player1Id);
-    await clubutils.approvePlayer(clubCode, ownerId, player2Id);
-    await clubutils.approvePlayer(clubCode, ownerId, player3Id);
-    await clubutils.approvePlayer(clubCode, ownerId, player4Id);
-
-    // join a game
-    await gameutils.joinGame(player1Id, game.gameCode, 1);
-    await gameutils.joinGame(player2Id, game.gameCode, 2);
-    await gameutils.joinGame(player3Id, game.gameCode, 3);
-    await gameutils.joinGame(player4Id, game.gameCode, 4);
-
-    // buyin
-    await gameutils.buyin(player1Id, game.gameCode, 100);
-    await gameutils.buyin(player2Id, game.gameCode, 100);
-    await gameutils.buyin(player3Id, game.gameCode, 100);
-    await gameutils.buyin(player4Id, game.gameCode, 100);
-
-    // request seat change
-    await gameutils.requestSeatChange(player1Id, game.gameCode);
-    await gameutils.requestSeatChange(player2Id, game.gameCode);
 
     // confirm seat change
-    await gameutils.confirmSeatChange(player1Id, game.gameCode, 5);
-    await gameutils.confirmSeatChange(player2Id, game.gameCode, 6);
+    const resp4 = await gameutils.confirmSeatChange(player1, game.gameCode, 3);
+    expect(resp4).toBe(true);
+    const resp5 = await gameutils.confirmSeatChange(player2, game.gameCode, 3);
+    expect(resp5).toBe(true);
 
-    // make seat change
-    // try {
-    //   await axios.post(
-    //     `${GAMESERVER_API}/handle-seat-change?gameCode=` + game.gameCode,
-    //     {}
-    //   );
-    // } catch (error) {
-    //   logger.error(error.toString());
-    //   expect(true).toBeFalsy();
-    // }
+    // wait for 6 seconds
+    await sleep(6000);
+
+    // call waitlistTimeoutExpired
+    const gameId = await gameutils.getGameById(game.gameCode);
+    const hostId = await handutils.getPlayerById(ownerId);
+    try {
+      await axios.post(
+        `${GAMESERVER_API}/timer-callback/gameId/${gameId}/playerId/${hostId}/purpose/SEATCHANGE_INPROGRESS`
+      );
+    } catch (err) {
+      console.error(JSON.stringify(err));
+      expect(true).toBeFalsy();
+    }
+
+    // get all requested seat changes
+    const resp6 = await gameutils.seatChangeRequests(ownerId, game.gameCode);
+    expect(resp6).toHaveLength(1);
+    expect(resp6[0].playerUuid).toBe(player2);
   });
 });
