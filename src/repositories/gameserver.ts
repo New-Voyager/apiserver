0a5b27f12b4c5822843765e901a73ebc20becfab
Yong Cho <yongjec@gmail.com>
2021-09-20 00:05:34 -0400

    Update game server assigning logic

--- src/repositories/gameserver.ts
+++ src/repositories/gameserver.ts
@@ -4,21 +4,71 @@ import {getLogger} from '@src/utils/log';
 import {getGameConnection, getGameRepository} from '.';
 import {Cache} from '@src/cache/index';
 import {fixQuery} from '@src/utils';
-import {GameStatus} from '@src/entity/types';
-import {EntityManager} from 'typeorm';
+import {GameServerStatus, GameStatus} from '@src/entity/types';
+import {EntityManager, Repository} from 'typeorm';
 
 const logger = getLogger('repositories::gameserver');
 
 class GameServerRepositoryImpl {
   public async getNextGameServer(): Promise<GameServer> {
     const gameServerRepository = getGameRepository(GameServer);
-    let gameServers = await gameServerRepository.find();
+    let gameServers = await gameServerRepository
+      .createQueryBuilder()
+      .where({
+        status: GameServerStatus.ACTIVE,
+      })
+      .orderBy('id', 'ASC')
+      .getMany();
+
     if (gameServers.length === 0) {
-      throw new Error('No game server is availabe');
+      throw new Error('No game server is available');
     }
 
     // get next game server that has not reached maximum number of games
-    return gameServers[0];
+    let nextServer = gameServers[0];
+    let noActiveGames = gameServers[0].noActiveGames;
+    for (const server of gameServers) {
+      if (server.noActiveGames < server.maxGames) {
+        return server;
+      }
+      if (server.noActiveGames < noActiveGames) {
+        nextServer = server;
+        noActiveGames = server.noActiveGames;
+      }
+    }
+
+    // Should only get here if all available servers are full.
+    // Just return the server that has the fewest games.
+    return nextServer;
+  }
+
+  public async gameAdded(
+    gameServerUrl: string,
+    transactionManager?: EntityManager
+  ) {
+    let gameServerRepo: Repository<GameServer>;
+    if (transactionManager) {
+      gameServerRepo = transactionManager.getRepository(GameServer);
+    } else {
+      gameServerRepo = getGameRepository(GameServer);
+    }
+
+    await gameServerRepo.increment({url: gameServerUrl}, 'noActiveGames', 1);
+    await gameServerRepo.increment({url: gameServerUrl}, 'noGamesHandled', 1);
+  }
+
+  public async gameRemoved(
+    gameServerUrl: string,
+    transactionManager?: EntityManager
+  ) {
+    let gameServerRepo: Repository<GameServer>;
+    if (transactionManager) {
+      gameServerRepo = transactionManager.getRepository(GameServer);
+    } else {
+      gameServerRepo = getGameRepository(GameServer);
+    }
+
+    await gameServerRepo.decrement({url: gameServerUrl}, 'noActiveGames', 1);
   }
 
   public async get(
