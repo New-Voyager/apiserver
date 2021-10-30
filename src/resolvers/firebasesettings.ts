35b4223dff1b5825f07f7a91b65c0ca1d131505f
Yong Cho <yongjec@gmail.com>
2021-10-29 20:17:09 -0400

    Remove authorization for firebase settings

--- src/resolvers/firebasesettings.ts
+++ src/resolvers/firebasesettings.ts
@@ -2,7 +2,7 @@ import {Firebase} from '@src/firebase';
 const resolvers: any = {
   Query: {
     firebaseSettings: async (parent, args, ctx, info) => {
-      return Firebase.getSettings(ctx.req.playerId);
+      return Firebase.getSettings();
     },
   },
 };
