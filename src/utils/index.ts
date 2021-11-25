d6d5b2f64f72d7c1a7838d430d2550edf75745b8
Yong Cho <yongjec@gmail.com>
2021-11-24 21:01:16 -0500

    result table, rake

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -145,3 +145,31 @@ export function centsToChips(cents: number): number {
   }
   return cents;
 }
+
+export function floorToNearest(num: number, nearest: number): number {
+  if (nearest === 1) {
+    return Math.floor(num);
+  }
+  num = Math.floor(num);
+  return num - (num % nearest);
+}
+
+export function floorDecimal(num: number, digits: number): number {
+  if (digits === 0) {
+    return Math.floor(num);
+  }
+  if (digits === 2) {
+    return Math.floor(num * 100) / 100;
+  }
+  throw new Error(`Unsupported digits in floorDecimal: ${digits}`);
+}
+
+export function roundDecimal(num: number, digits: number): number {
+  if (digits === 0) {
+    return Math.round(num);
+  }
+  if (digits === 2) {
+    return Math.round(num * 100) / 100;
+  }
+  throw new Error(`Unsupported digits in roundDecimal: ${digits}`);
+}
