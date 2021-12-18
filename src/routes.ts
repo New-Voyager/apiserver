2410dec5804ef8affe83e56c6ce93d04c5b2480c
Soma Natarajan <soma.voyager@gmail.com>
2021-12-18 15:45:21 -0500

    added backdoor api to login as a user

--- src/routes.ts
+++ src/routes.ts
@@ -55,7 +55,7 @@ export function addExternalRoutes(app: any) {
   const profile = getRunProfile();
   if (profile != RunProfile.PROD) {
     app.post('/auth/login-bot/:botName', loginBot);
-    app.post('/auth/login-bot/:playerId', loginPlayer);
+    app.post('/auth/login-player/:playerId', loginPlayer);
     app.get('/bot-script/game-code/:gameCode/hand/:handNum', generateBotScript);
     app.get(
       '/bot-script/debug/game-code/:gameCode/hand/:handNum',
